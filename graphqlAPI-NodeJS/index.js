781e81d3eecb9cbd1d62af2ed87d716e8413753d
NabajyotiModak <naba93981@gmail.com>
2023-02-01 17:16:34 +0530

    a basic graphql server uses typeDefs, resolvers

--- graphqlAPI-NodeJS/index.js
+++ graphqlAPI-NodeJS/index.js
@@ -1,7 +1,12 @@
 const { ApolloServer } = require("apollo-server");
+const { typeDefs } = require('./schema/type-defs');
+const {resolvers} = require('./schema/resolvers');
 
 const server = new ApolloServer({ typeDefs, resolvers });
+console.log(server);
 
-server.listen().then(({ url }) => {
+server.listen(5000).then(({ url }) => {
     console.log(`App is running at ${url}`);
+}).catch((err)=>{
+    console.log("cannot start");
 })
