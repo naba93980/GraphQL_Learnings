86d018f18de3e2f5134d96fcb38c18e613b6ac43
NabajyotiModak <naba93981@gmail.com>
2023-02-06 17:18:37 +0530

    updateUsername resolver defined

--- graphqlAPI-NodeJS/schema/resolvers.js
+++ graphqlAPI-NodeJS/schema/resolvers.js
@@ -26,13 +26,24 @@ const resolvers = {
         }
     },
 
-    Mutation : {
-        createUser: (parent, args) =>{
+    Mutation: {
+        createUser: (_parent, args) => {
             const user = args.input
-            const lastId = UserList[UserList.length - 1].id; 
-            user.id = lastId+1;
+            const lastId = UserList[UserList.length - 1].id;
+            user.id = lastId + 1;
             UserList.push(user);
             return user;
+        },
+        updateUsername: (_parent, args) => {
+            const { id, newUsername } = args.input;
+            let userUpdated;
+            UserList.forEach((user) => {
+                if (user.id === Number(id)) {
+                    user.username = newUsername;
+                    userUpdated = user
+                }
+            });
+            return userUpdated;
         }
     }
 }
