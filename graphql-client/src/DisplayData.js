df59b86a835db032c037a82fb5b55be58ccc3abd
NabajyotiModak <naba93981@gmail.com>
2023-02-08 02:47:02 +0530

    fetches movie onclick, takes user input to create new user through useMutation hook

--- graphql-client/src/DisplayData.js
+++ graphql-client/src/DisplayData.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useQuery, useLazyQuery, gql } from "@apollo/client";
+import { useQuery, useLazyQuery, gql, useMutation } from "@apollo/client";
 
 const QUERY_ALL_USERS = gql`
   query getAllUsers {
@@ -25,11 +25,31 @@ const GET_MOVIE_BY_NAME = gql`
   }
 `;
 
+const CREATE_USER = gql`
+  mutation createUser($input: CreateUserInput!) {
+    createUser(input: $input) {
+      name
+      age
+      nationality
+      username
+    }
+  }
+`
+
 export default function DisplayData() {
 
-  const { data: { users = [] } = [], loading } = useQuery(QUERY_ALL_USERS);
+  const { data: { users = [] } = [], loading, refetch } = useQuery(QUERY_ALL_USERS);
   const [fetchmovie, { data: movieSearchedData, error: movieError }] = useLazyQuery(GET_MOVIE_BY_NAME);
-  const [movieSearched, setMovieSearched] = useState("")
+  const [movieSearched, setMovieSearched] = useState("");
+  const [createUserMutation] = useMutation(CREATE_USER, {
+    onCompleted: refetch()
+  });
+
+  // Create User States
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [age, setAge] = useState(0);
+  const [nationality, setNationality] = useState("");
 
 
   if (loading) {
@@ -37,6 +57,23 @@ export default function DisplayData() {
   }
   return (
     <div>
+
+      <div>
+        <input type="text" placeholder="Name..." onChange={(e) => { setName(e.target.value) }} />
+        <input type="text" placeholder="Username..." onChange={(e) => { setUsername(e.target.value) }} />
+        <input type="number" placeholder="Age..." onChange={(e) => { setAge(Number(e.target.value)) }} />
+        <input type="text" placeholder="Nationality..." onChange={(e) => { setNationality(e.target.value.toUpperCase()) }} />
+        <button onClick={()=>createUserMutation({
+        variables: {
+          input: {
+            name,
+            username,
+            age,
+            nationality,
+          }
+        }
+        })}> Create User </button>
+      </div>
 
       {users.map((user) => {
         return (
@@ -58,11 +95,13 @@ export default function DisplayData() {
 
       <div>
         <input type="text" placeholder="movie here..." onChange={(e) => { setMovieSearched(e.target.value) }} />
-        <button onClick={()=>{fetchmovie(({
-          variables: {
-            name: movieSearched
-          }
-        }))}}> Fetch Data</button>
+        <button onClick={() => {
+          fetchmovie(({
+            variables: {
+              name: movieSearched
+            }
+          }))
+        }}> Fetch Data</button>
       </div>
 
       {movieSearchedData &&
@@ -73,10 +112,9 @@ export default function DisplayData() {
       }
 
       {
-        movieError && 
+        movieError &&
         <h2>There is some error fetching data</h2>
       }
     </div>
   )
 }
-
